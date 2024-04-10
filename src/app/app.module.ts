2f88de02e450a6fbf182236976bb77799a98572d
PalmerinErwerle <franiFJRF@gmail.com>
2024-04-10 16:32:25 +0200

    terminada vista de pagina de auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
 
+  isLogin = true;
+  isRegister = false;
+  isForgotPassword = false;
+
+  loginView() {
+    this.isLogin = true;
+    this.isRegister = false;
+    this.isForgotPassword = false;
+  }
+
+  registerView() {
+    this.isLogin = false;
+    this.isRegister = true;
+    this.isForgotPassword = false;
+  }
+
+  forgotPasswordView() {
+    this.isLogin = false;
+    this.isForgotPassword = true;
+  }
+
 }
