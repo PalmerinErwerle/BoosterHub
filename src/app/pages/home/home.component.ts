ff45ab4de348042ea953acf59918935ebbd30588
PalmerinErwerle <franiFJRF@gmail.com>
2024-04-24 10:46:13 +0200

    repaso y retoques de vista principal y navbars

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
   title = "BoosterHub";
+  screenWidth!: number;
+
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+    window.onresize = () => {
+      this.screenWidth = window.innerWidth;
+    };
+  }
+
 }
