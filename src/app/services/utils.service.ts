5f5af9fb9d51bfa5a9d22e7ed22c38d66bbba32c
PalmerinErwerle <franiFJRF@gmail.com>
2024-04-24 15:09:42 +0200

    creacion servicio para logica variada

---
+++ src/app/services/utils.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UtilsService {
+
+    router = inject(Router);
+
+    // Routing
+    routerLink(url: string) {
+        return this.router.navigateByUrl(url);
+    }
+
+    // LocalStorage POST
+    saveInLocalStorage(key: string, value: any) {
+        return localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    // LocalStorage GET
+    getFromLocalStorage(key: string) {
+        const value = localStorage.getItem(key);
+        if (value === null) {
+            return null;
+        }
+        return JSON.parse(value);
+    }
+
+}
