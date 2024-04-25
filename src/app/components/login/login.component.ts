7c20fa8bd17226e85fa9233ae91474f380c2b32d
PalmerinErwerle <franiFJRF@gmail.com>
2024-04-25 15:53:49 +0200

    login preliminar funcional

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DocumentData } from 'firebase/firestore';
+import { User } from 'src/app/models/user.model';
+import { FirestoreBaasService } from 'src/app/services/firestore-baas.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  firebaseService = inject(FirestoreBaasService);
+  utilsService = inject(UtilsService);
+
+  async submit() {
+    if (this.form.valid) {
+      this.firebaseService.signIn(this.form.value as User).then(res => {
+        this.getUserInfo(res.user.uid);
+      }).catch(er => {
+        console.log("Incorrect login data, try again.");
+      });
+    }
+  }
+
+  async getUserInfo(uid: string) {
+    if (this.form.valid) {
+      let path = 'users/' + uid;
+
+      this.firebaseService.getDocument(path).then((user: DocumentData | undefined) => {
+        if (user) {
+          const userData = user as User;
+          this.utilsService.saveInLocalStorage('user', userData);
+          this.utilsService.routerLink('/main/home');
+          this.form.reset();
+        } else {
+          console.log("error en el getDocument");
+        }
+
+        console.log("Bienvenido")
+      }).catch(er => {
+        console.log("Incorrect login data, try again.");
+      });
+    }
+  }
 
 }
