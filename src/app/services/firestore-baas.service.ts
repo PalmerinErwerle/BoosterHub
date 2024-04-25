75d078d50c31ed16edb5f25f92437440ce8fe589
PalmerinErwerle <franiFJRF@gmail.com>
2024-04-25 17:18:08 +0200

    update llamadas a BaaS

--- src/app/services/firestore-baas.service.ts
+++ src/app/services/firestore-baas.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, updateProfile } from 'firebase/auth';
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { getFirestore, doc, setDoc, getDoc, updateDoc, deleteDoc, addDoc, collection, collectionData, query } from '@angular/fire/firestore';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class FirestoreBaasService {
 
   auth = inject(AngularFireAuth);
   firestore = inject(AngularFirestore);
+  utilsService = inject(UtilsService);
 
 
   // AUTH
@@ -29,6 +31,17 @@ export class FirestoreBaasService {
     return createUserWithEmailAndPassword(getAuth(), user.email, user.password);
   }
 
+  // Actualizar user
+  updateUser(displayName: string | null | undefined) {
+    const currentUser = getAuth().currentUser;
+    if (currentUser) {
+        return updateProfile(currentUser, { displayName });
+    } else {
+        console.error("User is not signed in, cannot update profile");
+        return null;
+    }
+  }
+
   //Reset password
   sendRecoveryEmail(email: string) {
     return sendPasswordResetEmail(getAuth(), email);
@@ -38,6 +51,7 @@ export class FirestoreBaasService {
   signOut() {
     getAuth().signOut();
     localStorage.removeItem('user');
+    this.utilsService.routerLink('/auth');
   }
 
 
