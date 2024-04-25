9d7fe702a57f4eb13f8ef0820a1f5f0d966291bb
PalmerinErwerle <franiFJRF@gmail.com>
2024-04-25 15:53:26 +0200

    fix conexion con Firestore BaaS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material.module';
+
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA86qxyv-TB3wxEVx3jRHLLH7MzvlG7lNk",
+  authDomain: "boosterhub-frodfer716.firebaseapp.com",
+  projectId: "boosterhub-frodfer716",
+  storageBucket: "boosterhub-frodfer716.appspot.com",
+  messagingSenderId: "822863942503",
+  appId: "1:822863942503:web:662b1662304b5f7c6955c2"
+};
+
 
 @NgModule({
   declarations: [
@@ -26,9 +43,13 @@ import { MaterialModule } from './material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
