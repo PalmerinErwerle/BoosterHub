38a1fe74b50e0399bda6fde3b8a679bfc1409fa3
PalmerinErwerle <franiFJRF@gmail.com>
2024-04-29 17:08:05 +0200

    finalizacion logica principal forgot-password

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthComponent } from 'src/app/pages/auth/auth.component';
+import { FirestoreBaasService } from 'src/app/services/firestore-baas.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class ForgotPasswordComponent {
 
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  })
+
+  firebaseService = inject(FirestoreBaasService);
+  utilsService = inject(UtilsService);
+  authComponent = inject(AuthComponent);
+
+  async submit() {
+    if (this.form.valid) {
+      this.firebaseService.sendRecoveryEmail(this.form.value.email!).then(res => {
+        console.log("A password recovery mail has been sent to your email address.");
+
+        this.authComponent.loginView();
+        this.form.reset();
+      }).catch(er => {
+        console.log("There is no booster with that email address.");
+      })
+    }
+  }
+
 }
+
+
