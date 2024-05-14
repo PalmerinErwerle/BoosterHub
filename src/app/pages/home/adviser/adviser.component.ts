374845f4834f9b6f3de89d544eb1ed00ef4d0cf4
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-14 14:54:12 +0200

    fix dependencias component

--- src/app/pages/home/adviser/adviser.component.ts
+++ src/app/pages/home/adviser/adviser.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-adviser',
-  standalone: true,
-  imports: [],
   templateUrl: './adviser.component.html',
-  styleUrl: './adviser.component.scss'
+  styleUrls: ['./adviser.component.scss']
 })
 export class AdviserComponent {
 
