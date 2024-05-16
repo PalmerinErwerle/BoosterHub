d06d8f504411da1676e45b08e476d6e1f08e8bfa
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-17 01:37:38 +0200

    boton updateUser validado y funcional

--- src/app/pages/home/profile/profile.component.ts
+++ src/app/pages/home/profile/profile.component.ts
@@ -1,6 +1,13 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
+import { ToasterComponent } from 'src/app/components/toaster/toaster.component';
+import { RaiderIoCharacter } from 'src/app/models/raiderio-character.model';
 import { User } from 'src/app/models/user.model';
+import { FirestoreBaasService } from 'src/app/services/firestore-baas.service';
+import { RaiderIoService } from 'src/app/services/raider-io.service';
 import { UserService } from 'src/app/services/user.service';
 import { UtilsService } from 'src/app/services/utils.service';
 
@@ -14,10 +21,15 @@ export class ProfileComponent implements OnInit {
   loader = false;
   uid!: string;
   character!: User | null;
+  raiderIoData!: RaiderIoCharacter;
   raiderIoLink!: string;
   cards: any[] = [];
 
   route = inject(ActivatedRoute);
+  spinner = inject(SpinnerComponent);
+  toaster = inject(ToasterComponent);
+  firebaseService = inject(FirestoreBaasService);
+  raiderIo = inject(RaiderIoService);
   utilsService = inject(UtilsService);
   userService = inject(UserService);
 
@@ -39,19 +51,19 @@ export class ProfileComponent implements OnInit {
     this.cards = [
       {
         title: this.factionName(this.character?.character_faction),
-        image: '../../../../assets/img/characters/factions/'+ this.character?.character_faction +'.png'
+        image: '../../../../assets/img/characters/factions/' + this.character?.character_faction + '.png'
       },
       {
         title: this.character?.character_race,
-        image: '../../../../assets/img/characters/races/'+ this.character?.character_race +'.png'
+        image: '../../../../assets/img/characters/races/' + this.character?.character_race + '.png'
       },
       {
         title: this.character?.character_class,
-        image: '../../../../assets/img/characters/classes/'+ this.character?.character_class +'.png'
+        image: '../../../../assets/img/characters/classes/' + this.character?.character_class + '.png'
       },
       {
         title: this.roleName(this.character?.character_role),
-        image: '../../../../assets/img/characters/roles/'+ this.character?.character_role +'.png'
+        image: '../../../../assets/img/characters/roles/' + this.character?.character_role + '.png'
       },
       {
         title: Math.round(this.character?.character_ilevel as number),
@@ -80,6 +92,39 @@ export class ProfileComponent implements OnInit {
     } else {
       return "Healer"
     }
+  }
+
+  async updateUser(character_name: string | undefined, character_realm: string | undefined) {
+    let path = 'users/' + this.uid;
+
+    this.spinner.showSpinner();
+
+    this.raiderIo.getCharacter(character_name, character_realm, "eu")
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.toaster.errorToast('Unexpected error while loading the character, please try again');
+        this.spinner.hideSpinner(2000);
+        return throwError(() => new Error('Unexpected error while loading the character, please try again'));
+      })
+    ).subscribe(data => {
+        this.raiderIoData = data;
+
+        this.character!.character_faction = this.raiderIoData?.faction;
+        this.character!.character_race = this.raiderIoData?.race;
+        this.character!.character_role = this.raiderIoData?.active_spec_role.toLowerCase();
+        this.character!.character_class = this.raiderIoData?.class;
+        this.character!.character_ilevel = this.raiderIoData?.gear.item_level_equipped;
+        this.character!.character_rio = this.raiderIoData?.mythic_plus_scores_by_season[0].scores.all as unknown as string;
+
+        this.firebaseService.updateDocument(path, this.character).then(async () => {
+        }).catch(er => {
+          this.toaster.errorToast("Incorrect booster data gathering, try again");
+        });
+      });
+
+    this.toaster.successToast("User updated successfully");
+    this.spinner.hideSpinner(2000);
+    this.utilsService.reload("/home/profile/" + this.uid, 2000);
   }
 
 }
