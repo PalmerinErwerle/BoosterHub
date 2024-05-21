c421ca117b02873777d2e378f8dfce2ca31b56ae
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-21 11:02:07 +0200

    implementacion searchbar

--- src/app/components/mythic-list/mythic-list.component.ts
+++ src/app/components/mythic-list/mythic-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Mythic } from 'src/app/models/mythic.model';
 
 @Component({
@@ -6,9 +6,29 @@ import { Mythic } from 'src/app/models/mythic.model';
   templateUrl: './mythic-list.component.html',
   styleUrls: ['./mythic-list.component.scss']
 })
-export class MythicListComponent {
+export class MythicListComponent implements OnInit {
 
   loader = false;
+
   @Input() mythics!: Mythic[];
+  @Input() filterMythicsValue!: string;
+
+  filteredMythics!: Mythic[];
+
+  ngOnInit() {
+    this.filteredMythics = this.mythics;
+  }
+
+  filterMythic() {
+    this.filteredMythics = this.mythics.filter(mythic => {
+      // Recorrer las propiedades del objeto mythic y verificar si alguna coincide con filterValue
+      for (const property in mythic) {
+        if (typeof mythic.id === 'string' && mythic.id.toLowerCase().includes(this.filterMythicsValue.toLowerCase())) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
 
 }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -9,6 +9,26 @@ import { User } from 'src/app/models/user.model';
 export class UserListComponent {
 
   loader = false;
+
   @Input() users!: User[];
+  @Input() filterUsersValue!: string;
+
+  filteredUsers!: User[];
+
+  ngOnInit() {
+    this.filteredUsers = this.users;
+  }
+
+  filterUser() {
+    this.filteredUsers = this.users.filter(user => {
+      // Recorrer las propiedades del objeto mythic y verificar si alguna coincide con filterValue
+      for (const property in user) {
+        if (typeof user.character_name === 'string' && user.character_name.toLowerCase().includes(this.filterUsersValue.toLowerCase())) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
 
 }
