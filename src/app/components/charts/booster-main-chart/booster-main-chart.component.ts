f10ebdc93bc88a94152fab7552dee55acdd0f01a
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-23 14:53:30 +0200

    creacion BoosterMainChart

---
+++ src/app/components/charts/booster-main-chart/booster-main-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booster-main-chart',
+  templateUrl: './booster-main-chart.component.html',
+  styleUrls: ['./booster-main-chart.component.scss']
+})
+export class BoosterMainChartComponent {
+
+}
