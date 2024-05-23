239ff147513346277e6a5b8346beed0333dc3d50
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-23 14:53:40 +0200

    creacion AdviserMainChart

---
+++ src/app/components/charts/adviser-main-chart/adviser-main-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adviser-main-chart',
+  templateUrl: './adviser-main-chart.component.html',
+  styleUrls: ['./adviser-main-chart.component.scss']
+})
+export class AdviserMainChartComponent {
+
+}
