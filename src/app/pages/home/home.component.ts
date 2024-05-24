8432c899dd5e93d0125c16a8e330f39e5290d243
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-25 01:56:35 +0200

    implementada validacion UserRole en Sidenav

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
 import { AfterContentChecked, ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
 import { ToasterComponent } from 'src/app/components/toaster/toaster.component';
+import { User } from 'src/app/models/user.model';
 import { FirestoreBaasService } from 'src/app/services/firestore-baas.service';
+import { UserService } from 'src/app/services/user.service';
 import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
@@ -14,48 +16,66 @@ export class HomeComponent implements OnInit, AfterContentChecked {
   title!: string;
   screenWidth!: number;
   uid!: string;
+  user!: User;
 
   pages: any[] = [];
 
   firebaseService = inject(FirestoreBaasService);
   spinner = inject(SpinnerComponent);
   toaster = inject(ToasterComponent);
+  userService = inject(UserService);
   utilsService = inject(UtilsService);
   cdref = inject(ChangeDetectorRef);
 
-  ngOnInit() {
+  async ngOnInit() {
     this.screenWidth = window.innerWidth;
     window.onresize = () => {
       this.screenWidth = window.innerWidth;
     };
 
     this.uid = this.utilsService.getUserUid();
+    this.user = await this.userService.getUserByUid(this.uid) as User;
 
     this.pages = [
       {
         title: 'Home',
         url: '',
-        icon: 'home'
+        icon: 'home',
+        query1: 'booster',
+        query2: 'adviser',
+        query3: 'admin'
       },
       {
         title: 'My Profile',
         url: 'profile/' + this.uid,
-        icon: 'person'
+        icon: 'person',
+        query1: 'booster',
+        query2: 'adviser',
+        query3: 'admin'
       },
       {
         title: 'Booster Menu',
         url: 'booster-menu',
-        icon: 'work'
+        icon: 'work',
+        query1: 'booster',
+        query2: 'adviser',
+        query3: 'admin'
       },
       {
         title: 'Adviser Menu',
         url: 'adviser-menu',
-        icon: 'assignment_ind'
+        icon: 'assignment_ind',
+        query1: 'adviser',
+        query2: 'adviser',
+        query3: 'admin'
       },
       {
         title: 'Admin Menu',
         url: 'admin-menu',
-        icon: 'security'
+        icon: 'security',
+        query1: 'admin',
+        query2: 'admin',
+        query3: 'admin'
       }
     ]
   }
